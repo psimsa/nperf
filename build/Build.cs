9036019ee2bc48de7483aed04e9b24985fb90cf3
psimsa <pavel@simsa.cz>
2023-04-29 16:11:37 +0200

    Update build and command line (#3)
    * Enhance with command line options
    
    * Update build
    
    ---------
    
    Co-authored-by: Pavel Simsa <pavel.simsa@vista.com>

--- build/Build.cs
+++ build/Build.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Linq;
 using System.Security.Policy;
+using JetBrains.Annotations;
+using NuGet.Versioning;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.CI.GitHubActions;
@@ -11,23 +13,36 @@ using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
+using static System.Net.Mime.MediaTypeNames;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [GitHubActions(
-    "Build main",
+    "Continuous integration",
     GitHubActionsImage.UbuntuLatest,
-    OnPushBranches = new[] { "main" },
-    OnPullRequestBranches = new[] { "main" },
+    On = new [] { GitHubActionsTrigger.Push },
     InvokedTargets = new[]
     {
-        nameof(Clean), nameof(Compile), nameof(Publish)
-        // , nameof(Pack), nameof(PublishToGitHubNuget), nameof(Publish)
+        nameof(Clean), nameof(Compile), nameof(PublishBinary), nameof(Pack)
     },
-    // ImportSecrets = new[] { nameof(NuGetApiKey) },
     EnableGitHubToken = true)]
+[GitHubActions(
+    "Build main and publish to nuget",
+    GitHubActionsImage.UbuntuLatest,
+    OnPushBranches = new[] { "main" },
+    InvokedTargets = new[]
+        { nameof(Clean), nameof(Compile), nameof(Pack), nameof(PublishToGitHubNuget), nameof(Publish), nameof(PublishBinary) },
+    ImportSecrets = new[] { nameof(NuGetApiKey) },
+    EnableGitHubToken = true)]
+[GitHubActions(
+    "Manual publish to Github Nuget",
+    GitHubActionsImage.UbuntuLatest,
+    On = new[] { GitHubActionsTrigger.WorkflowDispatch },
+    InvokedTargets = new[] { nameof(Pack), nameof(PublishToGitHubNuget) },
+    EnableGitHubToken = true
+)]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
@@ -42,11 +57,17 @@ class Build : NukeBuild
 
     [Solution(GenerateProjects = true)] readonly Solution Solution;
 
-    // [Parameter][Secret] readonly string NuGetApiKey;
+    [Parameter][Secret] readonly string NuGetApiKey;
 
     GitHubActions GitHubActions => GitHubActions.Instance;
 
     [GitRepository] readonly GitRepository Repository;
+
+    [LatestNuGetVersion(
+        packageId: "dotnet-nperf",
+        IncludePrerelease = false)]
+    [CanBeNull]
+    readonly NuGetVersion DotnetNperfVersion;
 
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
@@ -75,8 +96,9 @@ class Build : NukeBuild
             );
         });
 
-    Target Publish => _ => _
+    Target PublishBinary => _ => _
         .DependsOn(Compile)
+        .DependsOn(Clean)
         .OnlyWhenStatic(() => GitHubActions.Instance != null)
         .Produces(ArtifactsDirectory / "nperf")
         .Executes(() =>
@@ -91,6 +113,67 @@ class Build : NukeBuild
                     .Add("-p:PublishAot=true")
                     .Add("-p:StripSymbols=true")
                 )
+            );
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Compile)
+        .DependsOn(Clean)
+        .Produces(ArtifactsDirectory / "*.nupkg")
+        .Executes(() =>
+        {
+            var currentVersion = DotnetNperfVersion ?? new NuGetVersion(0, 0, 0);
+
+            var newMajor = 1;
+            var newMinor = 0;
+            var newPatch = currentVersion.Patch + 1;
+
+            if (newMajor > currentVersion.Major)
+            {
+                newMinor = 0;
+                newPatch = 0;
+            }
+            else if (newMinor > currentVersion.Minor)
+            {
+                newPatch = 0;
+            }
+
+            var newVersion = new NuGetVersion(newMajor, newMinor, newPatch,
+                Repository.IsOnMainOrMasterBranch() ? null : $"preview{GitHubActions?.RunNumber ?? 0}");
+
+            DotNetPack(_ => _
+                .SetConfiguration(Configuration)
+                .SetOutputDirectory(ArtifactsDirectory)
+                .SetNoBuild(true)
+                .SetNoRestore(true)
+                .SetVersion(newVersion.ToString())
+                .SetVerbosity(DotNetVerbosity.Normal)
+                .SetProject(Solution.src.nperf)
+            );
+        });
+
+    Target PublishToGitHubNuget => _ => _
+        .DependsOn(Pack)
+        .Consumes(Pack)
+        .Executes(() =>
+        {
+            DotNetNuGetPush(_ => _
+                .SetTargetPath(ArtifactsDirectory / "*.nupkg")
+                .SetSource("https://nuget.pkg.github.com/psimsa/index.json")
+                .SetApiKey(GitHubActions.Token)
+            );
+        });
+
+    Target Publish => _ => _
+        .DependsOn(Pack)
+        .Consumes(Pack)
+        .Requires(() => NuGetApiKey)
+        .Executes(() =>
+        {
+            DotNetNuGetPush(_ => _
+                .SetTargetPath(ArtifactsDirectory / "*.nupkg")
+                .SetSource("https://api.nuget.org/v3/index.json")
+                .SetApiKey(NuGetApiKey)
             );
         });
 }
